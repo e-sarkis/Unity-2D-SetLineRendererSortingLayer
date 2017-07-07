2908cf38599d2101c7e32b455f986beb2cbf3539
E Sarkis <elisha.sarkis96@gmail.com>
2017-07-07 15:03:39 -0400

    Added script.

---
+++ SetLineRendererSortingLayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Sets SortingLayer of Component GameObject's LineRenderer Component.
+/// </summary>
+public class SetLineRendererSortingLayer : MonoBehaviour 
+{
+
+	public LineRenderer lr;
+	public string SortingLayer;
+	public int OrderInLayer;
+
+	void Awake() 
+	{
+		lr = this.GetComponent<LineRenderer>();
+	}
+
+	void Update() 
+	{
+		lr.sortingLayerName = SortingLayer;
+		lr.sortingOrder = OrderInLayer;
+	}
+		
+}
